c1c7ac7ce583e236a9e1327dffacbffa0a01d845
abhaydee <abhaygirish1997@gmail.com>
2024-03-17 00:59:23 -0400

    adding patch and google auth

--- server.js
+++ server.js
@@ -1,22 +1,40 @@
-let express = require("express");
-let app = express();
-const schema = require("./schema/schema.json");
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
 const Ajv = require("ajv");
+const schema = require("./schema/schema.json");
+const { redisConnection } = require("./controllers/redisConnection");
+const { generateETag } = require("./utils/generateEtag");
+
+const app = express();
 const ajv = new Ajv();
 
 const validate = ajv.compile(schema);
-app.use(express.json());
-const { redisConnection } = require("./controllers/redisConnection");
-const { generateETag } = require("./utils/generateEtag");
-let client = "";
-//generate etag
 
-//Adding new body to the datastore
-app.post("/plan", async (req, res) => {
+app.use(express.json());
+app.use(
+  session({
+    secret: "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",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+function ensureAuthenticated(req, res, next) {
+  const accessToken = req.headers.authorization;
+  if (!accessToken) {
+    return res.status(401).send("Unauthorized: Access token is required");
+  }
+  next();
+}
+
+let client = "";
+
+app.post("/plan", ensureAuthenticated, async (req, res) => {
   const { body } = req;
   const validCheck = validate(body);
   if (!validCheck) {
-    res.sendStatus(304);
+    res.status(400).send("Invalid request");
   } else {
     const redisData = await client.set(
       req.body.objectType + ":" + req.body.objectId,
@@ -25,12 +43,11 @@ app.post("/plan", async (req, res) => {
     let storedJsonData = await client.get(
       req.body.objectType + ":" + req.body.objectId
     );
-    console.log("stored json data", storedJsonData);
-    res.status(200).send(storedJsonData);
+    res.status(200).send(JSON.parse(storedJsonData));
   }
 });
 
-app.get("/plan", async (req, res) => {
+app.get("/plan", ensureAuthenticated, async (req, res) => {
   try {
     const keys = await client.keys("*");
     const values = await Promise.all(
@@ -38,13 +55,18 @@ app.get("/plan", async (req, res) => {
         return JSON.parse(await client.get(key));
       })
     );
-    res.status(200).send(values);
+    console.log("values", values);
+    if (values.length == 0) {
+      res.sendStatus(204);
+    } else {
+      res.status(200).send(values);
+    }
   } catch (error) {
     res.sendStatus(500);
   }
 });
-//get the json from the datastore
-app.get("/plan/:id", async (req, res) => {
+
+app.get("/plan/:id", ensureAuthenticated, async (req, res) => {
   const data = JSON.parse(await client.get(req.params.id));
 
   if (!data) {
@@ -52,29 +74,86 @@ app.get("/plan/:id", async (req, res) => {
   }
 
   const storedETag = generateETag(data);
-
   if (storedETag == null) {
-    console.log("hitting here");
     res.send(data);
   } else {
     const clientETag = req.headers["if-none-match"];
-    console.log(
-      "the client etag and the stored etag ",
-      typeof clientETag + " : " + storedETag
-    );
-    if (clientETag && clientETag.trim() == storedETag.trim()) {
-      console.log("hitting match");
+    if (clientETag) {
       res.sendStatus(304); // Not Modified
     } else {
-      console.log("hitting non match");
-      console.log("the stored etag", storedETag);
       res.send(data);
     }
   }
 });
+app.patch("/plan/:id", ensureAuthenticated, async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updatedBody = req.body;
 
-//delete the item from the datastore
-app.delete("/plan/:id", async (req, res) => {
+    const validCheck = validate(updatedBody);
+
+    if (!validCheck) {
+      return res.status(400).send("Invalid request");
+    }
+
+    const urlETag = req.headers["if-match"] || "";
+    if (!urlETag) {
+      return res.status(400).send({
+        message: "ETag not provided!",
+      });
+    }
+
+    const eTag = await generateETag(id);
+
+    if (eTag !== urlETag) {
+      res.setHeader("ETag", eTag);
+      return res.status(412).send({
+        message: "ETAG does not match",
+      });
+    }
+
+    // Get the existing data
+    const existingData = JSON.parse(await client.get(id));
+
+    // Create a map of existing linked plan services for efficient lookup
+    const existingLinkedPlanServicesMap = new Map(
+      existingData.linkedPlanServices.map((service) => [
+        service.objectId,
+        service,
+      ])
+    );
+
+    // Filter and update linked plan services
+    const updatedLinkedPlanServices = updatedBody.linkedPlanServices.reduce(
+      (accumulator, updatedService) => {
+        if (!existingLinkedPlanServicesMap.has(updatedService.objectId)) {
+          accumulator.push(updatedService); // Add new service
+        }
+        return accumulator;
+      },
+      [...existingData.linkedPlanServices]
+    ); // Initialize with existing services
+
+    // Update the existing data with filtered linked plan services
+    existingData.linkedPlanServices = updatedLinkedPlanServices;
+
+    // Set the updated object with the new key
+    await client.set(id, JSON.stringify(existingData));
+
+    const eTagNew = generateETag(id);
+    res.setHeader("ETag", eTagNew);
+
+    // Retrieve the updated data
+    const updatedData = JSON.parse(await client.get(id));
+
+    return res.status(200).send(updatedData);
+  } catch (error) {
+    console.error("Error updating data:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.delete("/plan/:id", ensureAuthenticated, async (req, res) => {
   const deleteData = await client.del(req.params.id);
   console.log("delete data", deleteData);
   if (deleteData == 1) {
@@ -84,8 +163,7 @@ app.delete("/plan/:id", async (req, res) => {
   }
 });
 
-//running the nodejs server
-let server = app.listen(8080, async function (req, res) {
+const server = app.listen(8080, async function () {
   console.log("the server is up and running on port 8080");
   client = await redisConnection();
 });
